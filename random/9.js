ec033c99ff288e1425f0d23a7994f303ee88cc9f
Lakshay saini <lakshaysaini2013@gmail.com>
2020-09-03 21:54:40 +0530

    js

--- random/9.js
+++ random/9.js
@@ -1,1 +1,11 @@
-/* Write a fuction that accepts a string. the function  */
+/* Write a fuction that accepts a string. the function  will capatlize the string*/
+
+function capitalize(str) {
+  words = [];
+  for (const word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(" ");
+}
+
+console.log(capitalize("the, name of the!"));
